33462ad43a0769175da2f83c9b8a0c8ced3e2af5
Ubuntu User <ubuntu@ik1-341-30869.vs.sakura.ne.jp>
2020-02-15 15:15:46 +0900

    20200215

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-import datetime
-
-date_today = datetime.date.today()
-date_today = str("{0:%Y%m%d}".format(date_today))
-
 #uri path list
 uri_path_list_csv=''
 
@@ -18,10 +13,10 @@ error_file=''
 vt_apikey = '' # apikey(VT)
 
 #slack channel
-slack_channel_new_malware='#test_malware'
-slack_channel_honeytrap ='#test_honeytrap_daily'
-slack_channel_wowhoneypot='#test_wowhoneypot_daily'
-slack_channel_wowhoneypotssl='#test_wowhoneypotssl_daily'
+slack_channel_new_malware='#'
+slack_channel_honeytrap ='#'
+slack_channel_wowhoneypot='#'
+slack_channel_wowhoneypotssl='#'
 
 #mode setting
-slack_flag='ON' # slack送信フラグ
+slack_flag='OFF' # slack送信フラグ ON/OFF
